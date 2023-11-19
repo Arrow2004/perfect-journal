46e4d03afda0be7b848061ed77797759ffee4779
unknown <a.abdusattorov@tuit.uz>
2023-11-19 18:38:58 +0300

    19.11.23 3

--- config/db.js
+++ config/db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose")
 
 const connectDB  = async ()=>{ mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
 }).then(res=>{
     console.log("Database connected.")
 }).catch(err=>{
